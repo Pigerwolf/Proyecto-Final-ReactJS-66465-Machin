e1cd72bd1db6a56249ee5a368878e8b76df11a9d
Hendersson Machin <henderssonconh@gmail.com>
2024-06-28 03:22:37 +0200

    Agregué un nuevo Logo (principal) textos en el header y ajusté tamaños

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import logo from '../components/Logo.png'; 
 import './Logo.css';
 
 const Logo = () => {
   return (
     
     <div className="container mt-4" id="top-logo">
-<img src={<Logo/>} alt='Logo de la Compañía'></img>
+<img src="https://lh5.googleusercontent.com/-iGnbA-5Oijk/TYf3SEXttDI/AAAAAAAACEU/LL_D-6jwWo4/s1600/Escape+Key.jpg" alt='Logo de la Compañía' className='Logo-Principal'></img>
       <div className="row">
+        <p className='p-dl-logo'>
+          Easy
+          Store
+          Computer
+        </p>
         <div className="col-md-7" id="searchbar">
           <div className="input-group mt-3">
             <input type="text" className="form-control" placeholder="Buscar..." aria-label="Search" aria-describedby="button-addon2" />

062275bf192aa54a06e994306144189382fdfb32
Hendersson Machin <henderssonconh@gmail.com>
2024-07-18 12:00:18 +0200

    corrigiendo fallas

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import Contacto from "./components/contacto"
 import MainContent from './components/MainContent';
 import Header from './components/Header';
 import Footer from "./components/Footer"
-import Catalogo from "./components/Catalogo.jsx";
+import Catalogo from "./components/Catalogo";
 import Products from './components/Products';
-import ProductPage from './components/productpage.jsx';
+import ProductPage from './components/productpage';
 
 
 function App() {

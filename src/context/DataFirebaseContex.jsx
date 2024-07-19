7a23a817537f03b9430f86c171fff8759380c25f
Hendersson Machin <henderssonconh@gmail.com>
2024-07-19 19:09:11 +0200

    se hizo un merge del origin al main en remote.

--- src/context/DataFirebaseContex.jsx
+++ src/context/DataFirebaseContex.jsx
@@ -1,22 +1,22 @@
-import { createContext, useRef, useState} from "react";
+import { createContext, useRef, useState } from "react";
 import useGetFirebase from "../hooks/useGetFirebase";
 
 const DataFirebaseContext = createContext()
 
 const DataFirebaseProvider = ({ children }) => {
 
-    const dataCloud = useGetFirebase()  
+    const dataCloud = useGetFirebase()
     const dataProductsRef = useRef(null)
-    dataProductsRef.current =  dataCloud
-    const [ dataCart, setDataCart ] = useState(JSON.parse(localStorage.getItem("cart")) || [])
+    dataProductsRef.current = dataCloud
+    const [dataCart, setDataCart] = useState(JSON.parse(localStorage.getItem("cart")) || [])
 
     const addProductToCart = (id) => {
 
-        const getProduct = dataProductsRef?.current?.find(( product ) => {
+        const getProduct = dataProductsRef?.current?.find((product) => {
             return product.id == id
         })
-        setDataCart(prevValue => [...prevValue, getProduct])   
-        localStorage.setItem("cart", JSON.stringify(dataCart))     
+        setDataCart(prevValue => [...prevValue, getProduct])
+        localStorage.setItem("cart", JSON.stringify(dataCart))
     }
 
     return (
